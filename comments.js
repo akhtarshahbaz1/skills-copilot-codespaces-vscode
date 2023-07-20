d66b5bcbc18b4921b10965a1f273bfb7c4c6fc5a
akhtarshahbaz1 <akhtar.shahbaz1@gmail.com>
2023-07-20 20:27:41 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,2 +1,12 @@
 // Create web server
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var Comment = mongoose.model('Comment');
 
+// Return a list of all comments
+router.get('/', function(req, res, next) {
+    Comment.find(function(err, comments){
+        if(err){ return next(err); }
+        res.json(comments);
+    })
